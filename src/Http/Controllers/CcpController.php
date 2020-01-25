ada6b051b8c7d1213229f219c7d7b2c67eb70ec2
Matt Burton <me@mattburton.co.uk>
2020-01-25 00:50:51 +0000

    Update CcpController.php

--- src/Http/Controllers/CcpController.php
+++ src/Http/Controllers/CcpController.php
@@ -4,16 +4,15 @@ namespace Techquity\CloudCommercePro\Http\Controllers;
 
 use Aero\Cart\Models\Order;
 use Aero\Catalog\Models\Category;
-
-
+use Aero\Catalog\Models\Variant;
 
 class CcpController
 {
     /**
-     * Send the given order to veeqo.
+     * Category list for CCP.
      *
-     * @param  int  $orderId
-     * @return Response
+     *
+     * @return Object
      */
     public function categories()
     {
@@ -34,6 +33,52 @@ class CcpController
 
         return response()->json(collect($return)->toJson());
 
+    }
+
+
+    /**
+     * Listings for CCP.
+     *
+     *
+     * @return Object
+     */
+    public function listings()
+    {
+        $return = [];
+
+        $variants = Variant::cursor()->filter(function ($variant) {
+            return $variant->id;
+        });
+
+        foreach ($variants as $variant) {
+
+            $return[$variant->id]['id'] = $variant->id;
+            $return[$variant->id]['sku'] = $variant->sku;
+            $return[$variant->id]['image'] = isset($variant->product->images->first()->file) ? trim(env('APP_URL').'/').($variant->product->images->first()->file) : null;
+            $return[$variant->id]['url'] = $variant->product->getUrl(true);
+            $return[$variant->id]['name'] = $variant->product->name;
+
+            $inc = setting('prices_inserted_inc_tax');
+
+            $return[$variant->id]['price'] = $inc ? ($variant->getPriceForQuantity(1)->sale_value_inc / 100) : ($variant->getPriceForQuantity(1)->sale_value_ex / 100);
+
+            if(!$inc){
+                $return[$variant->id]['vat_amount'] = (($variant->getPriceForQuantity(1)->sale_value_inc-$variant->getPriceForQuantity(1)->sale_value_ex) / 100);
+            }
+
+            $rate = ($variant->getPriceForQuantity(1)->sale_value_inc - $variant->getPriceForQuantity(1)->sale_value_ex) / $variant->getPriceForQuantity(1)->sale_value_ex * 100;
+
+            $return[$variant->id]['vat_rate'] = round($rate);
+            $return[$variant->id]['barcode'] = $variant->id;
+
+            $return[$variant->id]['attributes'] = $variant->attributes->map(function($attribute){
+                return [$attribute->group->name => $attribute->name];
+            })->values();
+
+            $return[$variant->id]['parent_ref'] = $variant->product->model;
+
+        }
+        return response()->json(collect($return)->toJson());
     }
 
 }
