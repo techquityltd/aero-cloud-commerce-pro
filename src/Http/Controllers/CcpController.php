a0f96e56a821db4209c53c50f91c533915fedc71
Matt Burton <me@mattburton.co.uk>
2020-01-26 22:51:34 +0000

    Added CSV export

--- src/Http/Controllers/CcpController.php
+++ src/Http/Controllers/CcpController.php
@@ -13,6 +13,71 @@ use Illuminate\Http\Request;
 
 class CcpController
 {
+    public function csv() {
+
+        $headers = array(
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=csv_export.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0"
+        );
+
+        $variants = Variant::whereHas('product', function ($query) {
+            $query->active();
+        })->select('id', 'sku', 'product_id', 'stock_level')
+            ->addSelect(['name' => Product::select('name')->whereColumn('products.id', 'variants.product_id')])
+            ->cursor();
+
+        $columns = array('parent_id', 'parent_ref', 'variant_id', 'sku' ,'barcode', 'manufacturer', 'name', 'summary', 'description', 'image', 'categories', 'tags', 'attributes', 'url', 'price', 'price_ex', 'vat_amount', 'vat_rate', 'stock');
+
+        $callback = function() use ($variants, $columns) {
+
+            $file = fopen('php://output', 'w');
+
+            fputcsv($file, $columns);
+
+            foreach($variants as $variant) {
+
+                // Pricing
+                $price_inc = ($variant->getPriceForQuantity(1)->sale_value_inc / 100);
+                $price_ex = ($variant->getPriceForQuantity(1)->sale_value_ex / 100);
+                $vat_amount = (($variant->getPriceForQuantity(1)->sale_value_inc-$variant->getPriceForQuantity(1)->sale_value_ex) / 100);
+                $vat_rate = ($variant->getPriceForQuantity(1)->sale_value_inc - $variant->getPriceForQuantity(1)->sale_value_ex) / $variant->getPriceForQuantity(1)->sale_value_ex * 100;
+
+                // Image
+                $image = isset($variant->product->images->first()->file) ? trim(env('APP_URL').'/').($variant->product->images->first()->file) : null;
+
+
+                // Categories
+                $categories = $variant->product->categories->map(function($category) {
+                    return collect($category->getAncestors()->pluck('name'))->push($category->name)->implode(' > ');
+                });
+                $categories = (implode($categories->toArray(), "|"));
+
+                // Tags
+                $tags = $variant->product->tags->map(function($tag) {
+                    return ($tag->group->name.':'.$tag->name);
+                });
+                $tags = (implode($tags->toArray(), "|"));
+
+                // Attributes
+                $attributes = $variant->attributes->map(function($attribute) {
+                    return ($attribute->group->name.':'.$attribute->name);
+                });
+                $attributes = (implode($attributes->toArray(), "|"));
+
+
+                fputcsv($file, array($variant->product_id, $variant->product->model, $variant->id, $variant->sku, $variant->barcode, $variant->product->manufacturer->name, $variant->product->name, $variant->product->summary, $variant->product->description, $image, $categories, $tags, $attributes, $variant->product->getUrl(true), $price_inc, $price_ex, $vat_amount, $vat_rate, $variant->stock_level));
+
+            }
+
+            fclose($file);
+        };
+
+        return response()->stream($callback, 200, $headers);
+    }
+
     /**
      * Update stock API for CCP.
      *
@@ -142,8 +207,6 @@ class CcpController
                 $return[$variant->id]['attributes'] = $variant->attributes->map(function($attribute){
                     return ['name' => $attribute->group->name, 'value' => $attribute->name];
                 })->values();
-
-
 
             }
         };
